74f7a94247bbd28df94115e7de4a3413d17fe619
Julien dewaele <julienstreetzer@localhost.localdomain>
2020-03-06 15:58:37 +0100

    [UPDATE] start server and a little bit error handling

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const express = require('express')
 const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = new Sequelize({
     dialect: 'sqlite',
@@ -6,11 +7,7 @@ const sequelize = new Sequelize({
 
 class User extends Model { }
 User.init({
-    firstname: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    lastname: {
+    name: {
         type: DataTypes.STRING,
         allowNull: false
     },
@@ -22,6 +19,11 @@ User.init({
         type: DataTypes.STRING,
         allowNull: false
     },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
     sexe: {
         type: DataTypes.STRING,
         allowNull: false
@@ -32,17 +34,20 @@ User.init({
     },
 }, { sequelize, modelName: 'user' });
 
-async function getUser(username) {
-    user.findOne({ where: {username: username} }).then(user => {
-        //console.log(user.id),
+async function getUser(name) {
+    user.findOne({ where: {name: name} }).then(user => {
         return user.id
     });
 }
 
-async function createUser(username, password) {
+async function createUser() {
     let user = await User.create({
-        username: username,
-        password: password
+        name: process.argv[2],
+        password: process.argv[3],
+        age: process.argv[4],
+        email: process.argv[5],
+        sexe: process.argv[6],
+        sexualorientation: process.argv[7]
     });
     return user;
 }
@@ -55,26 +60,42 @@ async function modifyPassword(user, password) {
 
 async function deleteUser(user) {
     await User.destroy({where: {
-          id: user.id,
+          email: user.email,
+          password: user.password
         }
     });
 }
 
+function startServer(app, port) {
+    app.get('/hello', (req, res) => {
+        res.send('world');
+        res.status(200).end();
+    });
+    app.get('/repeat-my-query', (req, res) => {
+        if (req.query.message) {
+            res.status(400).send('Bad Request');
+            return;
+        }
+        res.status(200).send(req.query.message);
+    });
+    app.listen(port, () => console.log(`Ready`))
+}
+
 async function main() {
+    const app = express();
+    const port = 2020;
+    startServer(app, port);
     try {
         await sequelize.sync();
         let user = null;
-        id = null;
-        if (process.argv[2] && !process.argv[3]) {
-            id = await getUser(process.argv[2]);
-            console.log(id);
+        //id = await getUser(process.argv[2]);
+        if (!process.argv[7]) {
+            console.log("\nToo few arguments(name password age email sexe sexual-orientation)\n");
             return;
         }
-        if (process.argv[3] && !process.argv[4])
-            user = await createUser(process.argv[2], process.argv[3]);
-        if (process.argv[4])
-            user = await modifyPassword(user, process.argv[4]);
-        await deleteUser(user);
+        user = await createUser();
+        //user = await modifyPassword(user, process.argv[4]);
+        //await deleteUser(user);
         console.log(user.toJSON());
     } catch (e) {
         console.error(e);
